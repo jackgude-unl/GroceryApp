e89b0ddc4444e015926e412607b537ae8871594a
MrBlueIce <nal-ramahy2@huskers.unl.edu>
2024-12-15 16:20:48 -0600

    Completed unit test for manager -test description dearch -test empty string - test name search

--- Engines/obj/Release/net7.0/Engines.AssemblyInfo.cs
+++ Engines/obj/Release/net7.0/Engines.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engines")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879c7787009c7c1cb7e611ccffe7dcd1d164793a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298f22eaff9d29b28073ccdd42f95cc60ca02f2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engines")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engines")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Unit Tests/UnitTest1.cs
+++ Unit Tests/UnitTest1.cs
@@ -1,11 +1,96 @@
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using Xunit;
+using Accessors.Classes;
+using Accessors.Interfaces;
+using Managers.Interfaces;
+using Engines;
+using Engines.Interfaces;
+using Managers;
+
+
 namespace Unit_Tests
 {
-    public class UnitTest1
+    public class ManagerTests
     {
         [Fact]
-        public void Test1()
+        public void SearchBar_DescriptionSearch()
         {
+            //Arrange
+            var mockProductAccessor = new Mock<IProductAccessor>();
+            var mockProductEngine = new Mock<IProductEngine>();
 
+            var products = new List<Product>
+            {
+                new Product(1, "French Bread", "Daily French break Frshinhly baked", (decimal)3.49),
+                new Product(2, "Apple", "Organic pink lady apples", (decimal)2.99),
+                new Product(3, "Ribeye", "Words can't describe how this steak", (decimal)1000.38),
+                new Product(4, "Milk", "The best whole milk the you have ever tasted :)", (decimal)100.99)
+            };
+
+            mockProductAccessor.Setup(x => x.GetAllProducts()).Returns(products);
+            mockProductEngine.Setup(x => x.SearchBar(products, "Steak")).Returns(products.Where(p => p.Description.Contains("steak", System.StringComparison.OrdinalIgnoreCase)).ToList());
+            var productManager = new ProductManager(mockProductAccessor.Object, mockProductEngine.Object);
+
+            //Act
+            var results = productManager.SearchBar("Steak");
+
+            //Assert
+            Assert.Single(results);
+            Assert.Equal("Ribeye", results.First().Name);
+        }
+
+        [Fact]
+        public void SearchBar_Empty()
+        {
+            //Arrange
+            var mockProductAccessor = new Mock<IProductAccessor>();
+            var mockProductEngine = new Mock<IProductEngine>();
+
+            var products = new List<Product>
+            {
+                new Product(1, "French Bread", "Daily French break Frshinhly baked", (decimal)3.49),
+                new Product(2, "Apple", "Organic pink lady apples", (decimal)2.99),
+                new Product(3, "Ribeye", "Words can't describe how this steak", (decimal)1000.38),
+                new Product(4, "Milk", "The best whole milk the you have ever tasted :)", (decimal)100.99)
+            };
+
+            mockProductAccessor.Setup(x => x.GetAllProducts()).Returns(products);
+            var productManager = new ProductManager(mockProductAccessor.Object, mockProductEngine.Object);
+
+            //Act
+            var results = productManager.SearchBar(string.Empty);
+
+            //Assert
+            Assert.Equal(4, results.Count);
+        }
+
+        [Fact]
+        public void SearchBar_NameSearch()
+        {
+            //Arrange
+            var mockProductAccessor = new Mock<IProductAccessor>();
+            var mockProductEngine = new Mock<IProductEngine>();
+
+            var products = new List<Product>
+            {
+                new Product(1, "French Bread", "Daily French break Frshinhly baked", (decimal)3.49),
+                new Product(2, "Apple", "Organic pink lady apples", (decimal)2.99),
+                new Product(3, "Ribeye", "Words can't describe how this steak", (decimal)1000.38),
+                new Product(4, "Milk", "The best whole milk the you have ever tasted :)", (decimal)100.99)
+            };
+
+            mockProductAccessor.Setup(x => x.GetAllProducts()).Returns(products);
+            mockProductEngine.Setup(x => x.SearchBar(products, "milk")).Returns(products.Where(p => p.Name.Contains("milk", System.StringComparison.OrdinalIgnoreCase)).ToList());
+            var productManager = new ProductManager(mockProductAccessor.Object, mockProductEngine.Object);
+
+            //Act
+            var results = productManager.SearchBar("milk");
+
+            //Assert
+            Assert.Single(results);
+            Assert.Equal("Milk", results.First().Name);
         }
     }
 }
